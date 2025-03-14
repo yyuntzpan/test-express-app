9e07dfdf536631e1b26cac978cc2b4db1df7b272
yyuntzpan <yap0602@gmail.com>
2025-03-14 23:15:45 +0800

    fix: add getGymsData()

--- index.js
+++ index.js
@@ -38,7 +38,66 @@ app.get("/api/test-db", async (req, res) => {
 app.get("/", (req, res) => {
   res.send("Express 應用程式運行中！訪問 /api/test-db 來測試資料庫連接");
 });
+const getFullGymData = async (req) => {
+  //關鍵字的參數
+  let keyword = req.query.keyword || "";
+  let feature = req.query.features || "";
+  let friendly = req.query.friendly || "";
 
+  let q_sql = " WHERE 1 "; //就算qs什麼都沒也會是1 = true
+  //篩選類別
+  const params = [];
+
+  if (feature) {
+    const featuresArray = req.query.features
+      .split(",")
+      .map((feature) => feature.trim());
+    q_sql += ` AND features.feature_name IN (${featuresArray
+      .map(() => "?")
+      .join(",")})`;
+    params.push(...featuresArray);
+  }
+
+  // 篩選關鍵字
+  if (keyword) {
+    // const keyword_ = db.escape(`%${keyword}%`);
+    // q_sql += ` AND (gym_name LIKE ${keyword_} OR gym_address LIKE ${keyword_}) `;
+    q_sql += ` AND (gym_name LIKE ? OR gym_address LIKE ? ) `;
+    params.push(`%${keyword}%`, `%${keyword}%`);
+  }
+
+  if (friendly === true || friendly === "true") {
+    q_sql += `AND is_elderly = ?`;
+    params.push(friendly);
+  }
+
+  const sql = `SELECT gyms.*, GROUP_CONCAT(DISTINCT features.feature_name) AS feature_list, GROUP_CONCAT( gym_images.image_filename) AS image_list FROM Gyms gyms LEFT JOIN GymFeatures AS gym_features ON gyms.gym_id = gym_features.gym_id JOIN Features AS features ON gym_features.feature_id = features.feature_id LEFT JOIN GymImages gym_images ON gyms.gym_id = gym_images.gym_id ${q_sql} GROUP BY gyms.gym_id order by gyms.gym_id desc;`;
+  try {
+    // 執行 SQL 查詢
+    const [rows] = await db.query(sql, params);
+    console.log(sql, params);
+    // 對每一行數據進行處理
+    const processedRows = rows.map((row) => {
+      return {
+        ...row, // 保留原有的所有屬性
+        feature_list: row.feature_list.split(","), // 將 feature_list 轉換為陣列.
+        image_list: row.image_list.split(","), // 將 image_list 轉換為陣列
+      };
+    });
+
+    console.log(processedRows);
+    return {
+      processedRows,
+      qs: req.query,
+    };
+  } catch (error) {
+    console.error("Error fetching gyms:", error);
+  }
+
+  console.log("有收到資料Feature:", req.query.features);
+  console.log("有收到資料Keyword:", req.query.keyword);
+  console.log("有收到資料SQL:", sql);
+};
 app.get("/gyms", async (req, res) => {
   let data = "";
   try {
